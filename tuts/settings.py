bed121a0eb4651c1a0b7f650e883edc4b646588b
unknown <dansong@JChen.YAI.ORG>
2014-08-20 12:34:36 -0400

    More changes to the tuts

--- tuts/settings.py
+++ tuts/settings.py
@@ -82,7 +82,22 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not os.environ.get("HOME") == 'C:\Documents and Settings\dansong\My Documents\portofolio\/tuts':
+if not os.environ.get("home") == 'C:\Documents and Settings\dansong':
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES['default'] = dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
--- tuts/wsgi.py
+++ tuts/wsgi.py
@@ -12,3 +12,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tuts.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+if os.environ.get('home') == 'C:\Documents and Settings\dansong':
+    from django.core.wsgi import get_wsgi_application
+    from dj_static import Cling
+
+    application = Cling(get_wsgi_application())
